1a9fea0db6cf71e9610b2ea1b1272c2d33830be2
martin <martin.conreras@edu.sait.ca>
2023-11-30 05:56:49 -0700

    slightly works

--- src/screens/tasks.jsx
+++ src/screens/tasks.jsx
@@ -1,52 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import examples from '../data/course_examples.json';
 
 export default function Tasks() {
-  const [events, setEvents] = useState([]);
+  const [currentDaySubjects, setCurrentDaySubjects] = useState([]);
+  const [currentDayOfWeek, setCurrentDayOfWeek] = useState('');
 
   useEffect(() => {
-    const newEvent = {
-      title: 'Example Event',
-      description: 'Description of the event',
-      date: new Date(),
-    };
+    const date = new Date();
+    const options = { weekday: 'long' };
+    const formattedDayOfWeek = date.toLocaleDateString('en-US', options);
+    setCurrentDayOfWeek(formattedDayOfWeek);
 
-    setEvents(prevEvents => [...prevEvents, newEvent]);
-  }, []);
-
-  const sortByDate = () => {
-    const sortedEvents = [...events].sort((a, b) => a.date - b.date);
-    setEvents(sortedEvents);
-  };
-
-  useEffect(() => {
-    sortByDate();
-  }, [events]);
-
-  // Filter events for the current day
-  const currentDayEvents = events.filter(event => {
-    const currentDate = new Date();
-    return (
-      event.date.getDate() === currentDate.getDate() &&
-      event.date.getMonth() === currentDate.getMonth() &&
-      event.date.getFullYear() === currentDate.getFullYear()
+    const filteredSubjects = examples.filter(
+      subject => subject.day === formattedDayOfWeek
     );
-  });
+    setCurrentDaySubjects(filteredSubjects);
+  }, []);
 
   return (
     <View>
-      <Text>Tasks Page</Text>
+      <Text>Subjects for the Current Day ({currentDayOfWeek})</Text>
       <View>
-        {/* Display events for the current day */}
-        {currentDayEvents.map((event, index) => (
+        {/* Display subjects for the current day */}
+        {currentDaySubjects.map((subject, index) => (
           <View key={index}>
-            <Text>{event.title}</Text>
-            <Text>{event.description}</Text>
-            <Text>{event.date.toString()}</Text>
-            <Text>----</Text>
+            <Text>Subject: {subject.subject}</Text>
+            <Text>Instructor: {subject.instructor}</Text>
+            <Text>Room: {subject.room}</Text>
+            <Text>Day: {subject.day}</Text>
+            <Text>Start Hour: {subject.startHour}</Text>
+            <Text>End Hour: {subject.endHour}</Text>
+            <Text>{'----'}</Text>
           </View>
         ))}
       </View>
     </View>
   );
 }
+
