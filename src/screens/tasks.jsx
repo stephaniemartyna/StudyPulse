a6c4e54627f0f2eb92a4bab56228b2a64a915255
martin <martin.conreras@edu.sait.ca>
2023-11-30 06:40:09 -0700

    it works!
    instal moment manually if it doesn't work: https://momentjs.com/

--- src/screens/tasks.jsx
+++ src/screens/tasks.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import examples from '../data/course_examples.json';
+import moment from 'moment';
 
 export default function Tasks() {
   const [currentDaySubjects, setCurrentDaySubjects] = useState([]);
   const [currentDayOfWeek, setCurrentDayOfWeek] = useState('');
 
   useEffect(() => {
-    const date = new Date();
-    const options = { weekday: 'long' };
-    const formattedDayOfWeek = date.toLocaleDateString('en-US', options);
+    const formattedDayOfWeek = moment().format('dddd');
     setCurrentDayOfWeek(formattedDayOfWeek);
 
     const filteredSubjects = examples.filter(
@@ -23,19 +22,22 @@ export default function Tasks() {
       <Text>Subjects for the Current Day ({currentDayOfWeek})</Text>
       <View>
         {/* Display subjects for the current day */}
-        {currentDaySubjects.map((subject, index) => (
-          <View key={index}>
-            <Text>Subject: {subject.subject}</Text>
-            <Text>Instructor: {subject.instructor}</Text>
-            <Text>Room: {subject.room}</Text>
-            <Text>Day: {subject.day}</Text>
-            <Text>Start Hour: {subject.startHour}</Text>
-            <Text>End Hour: {subject.endHour}</Text>
-            <Text>{'----'}</Text>
-          </View>
-        ))}
+        {Array.isArray(currentDaySubjects) && currentDaySubjects.length > 0 ? (
+          currentDaySubjects.map((subject, index) => (
+            <View key={index}>
+              <Text>Subject: {subject.subject}</Text>
+              <Text>Instructor: {subject.instructor}</Text>
+              <Text>Room: {subject.room}</Text>
+              <Text>Day: {subject.day}</Text>
+              <Text>Start Hour: {subject.startHour}</Text>
+              <Text>End Hour: {subject.endHour}</Text>
+              <Text>{'----'}</Text>
+            </View>
+          ))
+        ) : (
+          <Text>No subjects found for the current day</Text>
+        )}
       </View>
     </View>
   );
 }
-
